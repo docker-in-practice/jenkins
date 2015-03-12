d9b0290901bcd836cb8619037b1ce287b7ed1270
Ian Miell <ian.miell@gmail.com>
2015-03-12 14:58:44 +0000

    wget removed

--- swarm/startup.sh
+++ swarm/startup.sh
@@ -3,5 +3,9 @@ echo "Starting up sshd"
 mkdir -p /var/run/sshd
 /usr/sbin/sshd -D
 echo "Starting up swarm client with args: $@"
-java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar $@
+HOST_IP=$(ip route | ^default | awk '{print $3}')
+DOCKER_IP=${DOCKER_IP:-$HOST_IP}
+JENKINS_PORT=${JENKINS_PORT:-8080}
+JENKINS_LABELS=${JENKINS_LABELS:-""}
+java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar -labels "$JENKINS_LABELS" -master http://$DOCKER_IP:$JENKINS_PORT $@
 sleep infinity
