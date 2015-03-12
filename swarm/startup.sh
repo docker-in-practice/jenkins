169789c40cd2047e96665f6085291d911bd5e1f1
ianmiell <ian.miell@gmail.com>
2015-03-12 15:11:52 +0000

    show command

--- swarm/startup.sh
+++ swarm/startup.sh
@@ -7,5 +7,6 @@ HOST_IP=$(ip route | ^default | awk '{print $3}')
 DOCKER_IP=${DOCKER_IP:-$HOST_IP}
 JENKINS_PORT=${JENKINS_PORT:-8080}
 JENKINS_LABELS=${JENKINS_LABELS:-""}
+set -x
 java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar -labels "$JENKINS_LABELS" -master http://$DOCKER_IP:$JENKINS_PORT $@
 sleep infinity
