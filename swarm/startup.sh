af91fe555d66bb8c0b243b0992487e1cb8dd2b67
ianmiell <ian.miell@gmail.com>
2015-03-19 17:24:23 +0000

    IP

--- swarm/startup.sh
+++ swarm/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 HOST_IP=$(ip route | grep ^default | awk '{print $3}')
-DOCKER_IP=${DOCKER_IP:-$HOST_IP}
+JENKINS_IP=${JENKINS_IP:-$HOST_IP}
 JENKINS_PORT=${JENKINS_PORT:-8080}
 JENKINS_LABELS=${JENKINS_LABELS:-swarm}
 JENKINS_HOME=${JENKINS_HOME:-$HOME}
@@ -9,5 +9,5 @@ echo "$@"
 echo "and env:"
 echo "$(env)"
 set -x
-java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master http://$DOCKER_IP:$JENKINS_PORT $@
+java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master http://$JENKINS_IP:$JENKINS_PORT $@
 sleep infinity
