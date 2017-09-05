fabaa117f48e8be02ea0d2123a2480f8d9019954
Aidan Hobson Sayers <aidanhs@cantab.net>
2017-09-05 18:58:58 +0100

    Lots of fixes

--- swarm/startup.sh
+++ swarm/startup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-HOST_IP=$(ip route | grep ^default | awk '{print $3}')
-JENKINS_SERVER=${JENKINS_SERVER:-$HOST_IP}
-JENKINS_PORT=${JENKINS_PORT:-8080}
-JENKINS_LABELS=${JENKINS_LABELS:-swarm}
-JENKINS_HOME=${JENKINS_HOME:-$HOME}
+export HOST_IP=$(ip route | grep ^default | awk '{print $3}')
+export JENKINS_IP=${JENKINS_IP:-$HOST_IP}
+export JENKINS_PORT=${JENKINS_PORT:-8080}
+export JENKINS_LABELS=${JENKINS_LABELS:-swarm}
+export JENKINS_HOME=${JENKINS_HOME:-$HOME}
 echo "Starting up swarm client with args:"
 echo "$@"
 echo "and env:"
 echo "$(env)"
 set -x
-java -jar /home/jenkins_slave/swarm-client-1.22-jar-with-dependencies.jar -fsroot "$JENKINS_HOME" -labels "$JENKINS_LABELS" -master http://$JENKINS_SERVER:$JENKINS_PORT $@
-sleep infinity
+java -jar \
+/home/jenkins_slave/swarm-client-3.4.jar \
+    -sslFingerprints '[]' \
+    -fsroot "$JENKINS_HOME" \
+    -labels "$JENKINS_LABELS" \
+    -master http://$JENKINS_IP:$JENKINS_PORT "$@"
